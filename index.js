b9ebdb880f55a93f3c05bbc6cf9f33f48b66808b
nabilaghnaaa <nabilaghnaaa@gmail.com>
2025-10-21 14:53:00 +0700

    Endpoint PUT & DELETE

--- index.js
+++ index.js
@@ -62,3 +62,31 @@ app.post("/api/mahasiswa", (req, res) => {
     }
   );
 });
+
+app.put("/api/mahasiswa/:id", (req, res) => {
+  const userId = req.params.id;
+  const { nama, alamat, agama } = req.body;
+
+  db.query(
+    "UPDATE biodata SET nama = ?, alamat = ?, agama = ? WHERE id = ?",
+    [nama, alamat, agama, userId],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Database Error" });
+      }
+      res.json({ message: "User updated successfully" });
+    }
+  );
+});
+
+app.delete("/api/mahasiswa/:id", (req, res) => {
+  const userId = req.params.id;
+  db.query("DELETE FROM biodata WHERE id = ?", [userId], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ message: "Database Error" });
+    }
+    res.json({ message: "User deleted successfully" });
+  });
+});
