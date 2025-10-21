69074de8f5b9c879dfaeb48b74ab9dd40404854e
nabilaghnaaa <nabilaghnaaa@gmail.com>
2025-10-21 14:52:08 +0700

    Koneksi ke Database MySQL

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const mysql = require("mysql2");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,4 +12,20 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Banjarmasin67.",
+  database: "mahasiswa",
+  port: 3309,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database: " + err.stack);
+    return;
+  }
+  console.log("Koneksi Berhasil!");
 });
