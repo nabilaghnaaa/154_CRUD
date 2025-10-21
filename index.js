54cb3db5f931231122d08ed102c2794934f594a5
nabilaghnaaa <nabilaghnaaa@gmail.com>
2025-10-21 14:54:03 +0700

    perbaikan

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 const PORT = 3000;
-const mysql = require("mysql2");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
 
 const db = mysql.createConnection({
@@ -89,4 +85,8 @@ app.delete("/api/mahasiswa/:id", (req, res) => {
     }
     res.json({ message: "User deleted successfully" });
   });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
